d40ad64519088bb742662911b2f63bb3d5ac0af3
Alex Motarykin <motarykin@goozix.com>
2016-12-13 23:46:46 +0300

    Added all JOINs.

--- src/clj/dsl/core.clj
+++ src/clj/dsl/core.clj
@@ -1,6 +1,8 @@
 (ns dsl.core
   (:use [dsl.helper]
-        [dsl.render])
+        [dsl.render]
+        [clojure.template])
+
   (:require [clojure.java.jdbc :as jdbc]
             [mpnews.log :as logger]))
 
@@ -13,31 +15,7 @@
 (defn fetch-one [db relation]
   (first (fetch-all db relation)))
 
-;-----------
-
-(defn- join* [{:keys [tables joins] :as q} type alias table on]
-  (let [a (or alias table)]
-    (assoc q
-      :tables (assoc tables a table)
-      :joins (conj (or joins []) [a type on]))))
-
-(defn join-cross
-  ([q table] (join* q :cross table table nil))
-  ([q table alias] (join* q :cross table alias nil)))
-
-;-----------
- 
-(defn limit [relation v]
-  (assoc relation :limit v))
-
-(defn fields [query fd]
-  (assoc query :fields fd))
-
-(defn from
-  ([q table] (join* q nil table table nil))
-  ([q table alias] (join* q nil table alias nil)))
-
-;-----------
+;---- WHERE(EXPRESSION) -------
 
 (defn where* [query expr]
   (assoc query :where (conj-expression (:where query) expr)))
@@ -57,7 +35,45 @@
   [q body]
   `(where* ~q ~(prepare-expression body)))
 
-;-----------
+;----- JOINS ------
+
+(defn join* [{:keys [tables joins] :as q} type alias table on]
+  (let [a (or alias table)]
+    (assoc q
+      :tables (assoc tables a table)
+      :joins (conj (or joins []) [a type on]))))
+
+; этот код разворачивается в 5 объявлений макросов
+(do-template [join-name join-key] 
+
+  ; сам шаблон
+  (defmacro join-name
+    ([relation alias table cond]
+     `(join* ~relation ~join-key ~alias ~table ~(prepare-expression cond)))
+    ([relation table cond]
+     `(let [table# ~table]
+        (join* ~relation ~join-key nil table# ~(prepare-expression cond)))))
+
+  ; значения для параметров
+  join-inner :inner,
+  join :inner,
+  join-right :right,
+  join-left :left,
+  join-full :full)
+
+;----- SIMPLE ------
+ 
+(defn limit [relation v]
+  (assoc relation :limit v))
+
+(defn fields [query fd]
+  (assoc query :fields fd))
+
+(defn from
+  ([q table] (join* q nil table table nil))
+  ([q table alias] (join* q nil table alias nil)))
+
+;---- MAIN -------
 
 (defmacro select
   [& body]
